40dd53284eaaf281e9718f36e0a5253a7908ab52
kirue129 <kirue129.develop@gmail.com>
2016-12-21 19:20:47 +0900

    サンプルコード

---
+++ KinectForNUIWithWPFControl/DragDropDecorator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Kinect.Toolkit.Input;
+using Microsoft.Kinect.Wpf.Controls;
+using System.Windows.Controls;
+
+namespace KinectForNUIWithWPFControl
+{
+    class DragDropDecorator : Decorator, IKinectControl
+    {
+        public bool IsManipulatable
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public bool IsPressable
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public IKinectController CreateController(IInputModel inputModel, KinectRegion kinectRegion)
+        {
+            return new DragDropDecoratorController(inputModel, kinectRegion);
+
+        }
+    }
+}
--- KinectForNUIWithWPFControl/MainWindow.xaml.cs
+++ KinectForNUIWithWPFControl/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Wpf.Controls;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KinectForNUIWithWPFControl
 {
@@ -23,6 +13,37 @@ namespace KinectForNUIWithWPFControl
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void InitializeKinectControls()
+        {
+            KinectRegion.SetKinectRegion(this, kinectRegion);
+            this.kinectRegion.KinectSensor = KinectSensor.GetDefault();
+            this.kinectRegion.KinectSensor.Open();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Content = "Pressed!";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeKinectControls();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (kinectRegion != null)
+            {
+                if (kinectRegion.KinectSensor != null)
+                {
+                    kinectRegion.KinectSensor.Close();
+                    kinectRegion.KinectSensor = null;
+                }
+                kinectRegion = null;
+            }
         }
     }
 }
